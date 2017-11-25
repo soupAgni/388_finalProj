a3b79b800ecf83a3f10db38b4951b9d61572bd9d
Souparni Agnihotri <souparni@iastate.edu>
2017-11-25 11:36:56 -0600

    Can successfully download images locally!

--- FotInspiron_App/app/src/main/java/com/example/anastasia/fotinspiron_app/UserList.java
+++ FotInspiron_App/app/src/main/java/com/example/anastasia/fotinspiron_app/UserList.java
@@ -1,10 +1,18 @@
 package com.example.anastasia.fotinspiron_app;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.Window;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SearchView;
 
@@ -13,6 +21,7 @@ import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -77,4 +86,47 @@ public class UserList extends AppCompatActivity implements SearchView.OnQueryTex
     public boolean onQueryTextSubmit(String query) {
         return false;
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_users_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        //allow user to share the image
+        if(id == R.id.share){
+
+            //to get image change intent of the app so that it can be accessed through another activity
+            Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(i, 1);
+
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //getting the image from the activity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            try {
+                Bitmap bitmapImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+
+                Log.i("AppInfo", "ImageRecieved");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
 }
