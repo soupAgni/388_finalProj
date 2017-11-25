8879c87882f887262f2555444f5cbe116e53efa0
Souparni Agnihotri <souparni@iastate.edu>
2017-11-25 10:44:23 -0600

    Search view feature for userslist implemented!! :)

--- FotInspiron_App/app/src/main/java/com/example/anastasia/fotinspiron_app/UserList.java
+++ FotInspiron_App/app/src/main/java/com/example/anastasia/fotinspiron_app/UserList.java
@@ -2,8 +2,11 @@ package com.example.anastasia.fotinspiron_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -13,20 +16,25 @@ import com.parse.ParseUser;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UserList extends AppCompatActivity {
+public class UserList extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     ArrayList<String> usernames;
     ArrayAdapter arrayAdapter;
+    SearchView searchview;
+
+     ListView userList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
         setContentView(R.layout.activity_user_list);
 
+        searchview = findViewById(R.id.search);
          usernames = new ArrayList<String>();
 
          arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, usernames);
 
-        final ListView userList = findViewById(R.id.lv_userslist);
+         userList = findViewById(R.id.lv_userslist);
         //getting the query from the parse users class
         ParseQuery<ParseUser> query = ParseUser.getQuery();
         //You dont want it to get your own username
@@ -47,5 +55,26 @@ public class UserList extends AppCompatActivity {
                 }
             }
         });
+        userList.setTextFilterEnabled(true);
+        setupSearchView();
+    }
+    private void setupSearchView(){
+        searchview.setIconifiedByDefault(false);
+        searchview.setOnQueryTextListener(this);
+        searchview.setSubmitButtonEnabled(true);
+        searchview.setQueryHint("Search Here");
+    }
+    @Override
+    public boolean onQueryTextChange(String newText){
+        if(TextUtils.isEmpty(newText)){
+            userList.clearTextFilter();
+        }
+        else{
+            userList.setFilterText(newText.toString());
+        }
+        return true;
+    }
+    public boolean onQueryTextSubmit(String query) {
+        return false;
     }
 }
